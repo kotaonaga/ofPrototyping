d69e7bf057c8956ffd82548666cf4d2cfe802cfa
Kota Onaga <kohtaonaga@gmail.com>
2020-04-11 04:52:57 +0900

    円ひとつひとつの回転スピードを変えた

--- gradientShake/src/ofApp.cpp
+++ gradientShake/src/ofApp.cpp
@@ -1,14 +1,11 @@
 #include "ofApp.h"
 
-
-
 void ofApp::setup(){
     ofSetWindowShape(600, 600);
     ofSetRectMode(OF_RECTMODE_CENTER);
     ofSetCircleResolution(64);
     gui.setup();
     gui.add(radius.setup("radius", 20, 10, 90));
-    gui.add(minRadius.setup("min radius", 10, 10, 90));
     gui.add(hue[0].setup("circle1 color", 1, 0, 255));
     gui.add(hue[1].setup("circle2 color", 60, 0, 255));
     gui.add(hue[2].setup("circle3 color", 146, 0, 255));
@@ -20,12 +17,9 @@ void ofApp::setup(){
 }
 
 void ofApp::update(){
-//    for(int i = 0; i < circleNum; i++){
-//        rotateSpeed[i] += rotateSpeed[i];
-//    }
-    rotateSpeed[0] += rotateSpeed[0];
-    if(rotateSpeed[0] > 100) rotateSpeed[0] = 0.0;
-    cout << rotateSpeed[0] << endl;
+    for(int i = 0; i < circleNum; i++){
+        angle[i] += rotateSpeed[i];
+    }
     theta += 0.05;
 }
 
@@ -36,68 +30,65 @@ void ofApp::draw(){
     for(int i = 0; i < circleNum; i++){
         ofPushMatrix();
         ofTranslate(ofGetWidth()/2, ofGetHeight()/2);
-//        ofRotate(glm::radians(rotateSpeed[i] + startPos[i]));
-        ofRotateRad(rotateSpeed[i]);
-        
+        ofRotate(angle[i] + startPos[i]);
         circles[i].setRadius(radius * abs(sin(theta)) + 90);
-        cout << circles[0].getRadius() << endl;
-            circles[i].setHue(hue[i]);
-            circles[i].draw();
+        circles[i].setHue(hue[i]);
+        circles[i].draw();
         ofPopMatrix();
     }
     gui.draw();
 }
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseEntered(int x, int y){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseExited(int x, int y){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::gotMessage(ofMessage msg){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+    
 }
--- gradientShake/src/ofApp.h
+++ gradientShake/src/ofApp.h
@@ -6,23 +6,23 @@
 #define circleNum 4
 
 class ofApp : public ofBaseApp{
-
-	public:
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
     
     ofxPanel gui;
     ofxFloatSlider radius;
@@ -30,8 +30,10 @@ class ofApp : public ofBaseApp{
     ofxIntSlider hue[circleNum];
     Circle circles[circleNum];
     
-    float rotateSpeed[circleNum] = {0.1, 0.2,0.3, 0.4};
+    float rotateSpeed[circleNum] = {0.4, 0.8,1.2, 1.6};
+    float angle[circleNum];
     float theta = 0;
     int startPos[circleNum] = {0, 10, 20, 30};
     int rectAngle;
+    
 };
