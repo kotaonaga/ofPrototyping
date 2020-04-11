3c87fbfa06a32eceaf88e96e780c0fbd332bf0df
Kota Onaga <kohtaonaga@gmail.com>
2020-04-12 06:16:52 +0900

    顔認識機能つけた

--- typeFace/src/ofApp.cpp
+++ typeFace/src/ofApp.cpp
@@ -7,15 +7,43 @@ void ofApp::setup(){
     settings.antialiased = true;
     settings.addRanges(ofAlphabet::Japanese);
     font.load(settings);
-}
-
-//--------------------------------------------------------------
-void ofApp::update(){
+    
+    //haar
+    camera.setup(640, 480);
+    
+    color.allocate(camera.getWidth(), camera.getHeight());
+    gray.allocate(camera.getWidth(), camera.getHeight());
+    
+    haar.setup("haarcascade_frontalface_default.xml");
+    haar.setScaleHaar(3);
     
 }
 
 //--------------------------------------------------------------
+void ofApp::update(){
+    camera.update();
+    
+    if (camera.isFrameNew()){
+        color.setFromPixels(camera.getPixels());
+        gray = color;
+        haar.findHaarObjects(gray);
+    }
+}
+
+//--------------------------------------------------------------
 void ofApp::draw(){
+    
+    color.draw(0, 0);
+    
+    for (int i = 0; i < haar.blobs.size(); i++) {
+        ofSetColor(255);
+        ofNoFill();
+        ofDrawEllipse(haar.blobs[i].boundingRect.x,
+                      haar.blobs[i].boundingRect.y,
+                      haar.blobs[i].boundingRect.width,
+                      haar.blobs[i].boundingRect.height
+                      );
+    }
     ofSetColor(255);
     for(int i = 0; i< 45; i++){
         font.drawString("あ", 100 + ofRandom(-20, 20), 400 + ofRandom(-20, 20));
