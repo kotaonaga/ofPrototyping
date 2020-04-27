52f872e190b362ce9553fb605812a9fb4688ae85
Kota Onaga <kohtaonaga@gmail.com>
2020-04-27 18:45:00 +0900

    Eyeクラスを追加

---
+++ encouragedComputer/src/Eye/Eye.cpp
@@ -0,0 +1,8 @@
+//
+//  Eye.cpp
+//  encouragedComputer
+//
+//  Created by kota on 2020/04/27.
+//
+
+#include "Eye.hpp"
---
+++ encouragedComputer/src/Eye/Eye.hpp
@@ -0,0 +1,13 @@
+//
+//  Eye.hpp
+//  encouragedComputer
+//
+//  Created by kota on 2020/04/27.
+//
+
+#ifndef Eye_hpp
+#define Eye_hpp
+
+#include <stdio.h>
+
+#endif /* Eye_hpp */
